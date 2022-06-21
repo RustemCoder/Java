334401be20f89c5e7f9082c4adacd2c90139e71a
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-06-21 13:52:10 -0500

    Update HashMap.java

--- Cores/HashMap.java
+++ Cores/HashMap.java
@@ -1,13 +1,23 @@
 import java.util.HashMap;
 public class MyClass {
     public static void main(String args[]) {
-      
-      HashMap<Integer,Integer> hashMap = new HashMap<>();
-      hashMap.put(1,1);
+    
+    HashMap<Integer,Integer> hashMap = new HashMap<>();
+    hashMap.put(1,20);
       
       for(Integer x: hashMap.keySet()){
           System.out.println(x);
       }
       
+    System.out.println(hashMap.get(1));
+        
+    
+    // remove a value by a key 
+    System.out.println(hashMap.remove(1));
+    System.out.println("Size of hashMap is " + hashMap.size());
+    
+    // To remove all items
+    hashMap.clear();
+    
     }
 }
