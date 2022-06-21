2f5edf2dee365bc6e2972c53f9464b0670b5c899
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-06-21 14:23:14 -0500

    Update HashMap.java

--- Cores/HashMap.java
+++ Cores/HashMap.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 public class MyClass {
     public static void main(String args[]) {
     
@@ -11,7 +12,11 @@ public class MyClass {
       
     System.out.println(hashMap.get(1));
         
-    
+    // Iterate the map using
+    // for-each loop
+    for (Map.Entry<Integer, Integer> e : hashMap.entrySet())
+            System.out.println("Key: " + e.getKey()
+                               + " Value: " + e.getValue());
     // remove a value by a key 
     System.out.println(hashMap.remove(1));
     System.out.println("Size of hashMap is " + hashMap.size());
